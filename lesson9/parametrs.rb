535b4b2f044b668ad35bd8f42428f1d5d52f742c
fabio <a.p4nova@yandex.ru>
2018-01-04 00:11:06 +0300

    Параметры. Метод pop (удаление из массива последнего составляющего)

---
+++ lesson9/parametrs.rb
@@ -0,0 +1,17 @@
+puts "я строка 1"
+
+puts
+
+puts("я строка 2")
+
+array = ["James Bond", "Bradd Pit", "Dr. Who", "House MD", "Madonna", "Mr. Putin"]
+
+puts array.to_s
+
+array.pop #удаляет последний элеммент из массива
+
+puts array.to_s
+
+array.pop(2)
+
+puts array.to_s
