55f2f05ca052336b8ad297ae3212df2655e30f00
fabio <a.p4nova@yandex.ru>
2018-01-05 16:15:11 +0300

    Определяет, число или не число введено

--- lesson9/is_a.rb
+++ lesson9/is_a.rb
@@ -1,13 +1,12 @@
-value = ARGV[0].to_i
+value = gets.to_i
 
-if (value == "")
-	value = 0
-end
+#puts value.class 
+# if (value.is_a?(Numeric) == true)
+# это не работает тк to_i превращает текст в 0, 
+#а если без него - данные остаются в "" и читаются как String
 
-if (value.is_a?(Numeric))
+if (value != 0)
 	puts "Это число"
 else
-	puts "Это не число"
+	puts "Это не число, или ноль."
 end
-
-puts "Полученный агрумент - #{value}."
