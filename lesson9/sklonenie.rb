1087588d7870ed3e5cadf9fe7bcf01e6cc47ece6
apperen <a.p4nova@yandex.ru>
2018-01-07 23:55:25 +0300

    Негритята исправление

--- lesson9/sklonenie.rb
+++ lesson9/sklonenie.rb
@@ -1,28 +1,23 @@
-def sklonenie (number, krokodil, krokodila, krokodilov)
-	if (number == nil || !number.is_a?(Numeric))
-		number = 0
-	end
+def sklonenie(number, krokodil, krokodila, krokodilov)
+  if (number == nil || !number.is_a?(Numeric))
+    number = 0
+  end
 
-	ostatok = number % 10
+  ostatok = number % 10
 
-	if (ostatok == 1)
-		return krokodil
-	end
+  if (ostatok == 1) #
+    return krokodil
+  end
 
-	if (ostatok >= 2 && ostatok <= 4)
-		return krokodola
-	end
+  if (ostatok >= 2 && ostatok <= 4))
+    return krokodila
+  end
 
-	if (ostatok > 4 || ostatok == 0)
-		return krokodilov
-	end
+  if (ostatok >= 5 && ostatok <= 9 || ostatok == 0)
+    return krokodilov
+  end
 end
 
 skolko = ARGV[0].to_i
-
-puts "#{skolko} " + 
-	sklonenie(skolko, "негритёнок", "негритёнка", "негритят") + 
-	" " + 
-	sklonenie(skolko, "пошёл", "пошли", "пошли") + 
-	" купаться в море!"
-
+puts "#{skolko} #{sklonenie(skolko, "негритёнок", "негритёнка", "негритят")} " +
+     "#{sklonenie(skolko, "пошел", "пошли", "пошли")} купаться в море!"
