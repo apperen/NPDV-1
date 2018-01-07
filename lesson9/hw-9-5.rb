db929f56304e0a051546a8a1902b59a889b83977
apperen <a.p4nova@yandex.ru>
2018-01-07 23:47:22 +0300

    Угадай число от 0 до 16. Игра

---
+++ lesson9/hw-9-5.rb
@@ -0,0 +1,32 @@
+def etching(choice, key, victory, count)
+	if (choice == key)
+		abort victory
+	else
+		if (choice > key)
+			where = "нужно меньше"
+		else 
+			where = "нужно больше"
+		end
+
+		if ((key - choice).abs <= 2)
+			puts "Тепло, #{where}"
+		else
+			puts "Холодно, #{where}"
+		end
+	end
+end
+
+key = rand(15)
+victory = "Ура, вы выиграли! Это действительно число #{key}"
+
+puts "Загадано число от 0 до 16, отгадайте какое?"
+puts "Загадано число #{key}"
+
+count = 1
+while (count <= 3)
+	choice = gets.to_i
+	etching(choice, key, victory, count)
+	count += 1
+end
+
+puts "Вы проиграли, увы."
