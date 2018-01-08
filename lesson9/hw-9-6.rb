2e25b8e081efd3e726190adc0fadca722ed51c06
apperen <a.p4nova@yandex.ru>
2018-01-08 18:47:49 +0300

    10 негритят пошли капаться в море с проверкой чисел

--- lesson9/hw-9-6.rb
+++ lesson9/hw-9-6.rb
@@ -1,38 +1,38 @@
 def sklonenie (number, krokodil, krokodila, krokodilov)
-	if (number == nil || !number.is_a?(Numeric))
-		number = 0
-	end
+  if (number == nil || !number.is_a?(Numeric))
+    number = 0
+  end
 
-	ostatok = number % 10
+  ostatok = number % 10
 
-	if (number == 11 || number == 12 || number == 13 || number == 14)
-			return krokodilov
-	else
-		if (ostatok == 1)
-			return krokodil
-		end
+  if (number == 11 || number == 12 || number == 13 || number == 14)
+      return krokodilov
+  else
+    if (ostatok == 1)
+      return krokodil
+    end
 
-		if (ostatok >= 2 && ostatok <= 4)
-			return krokodola
-		end
+    if (ostatok >= 2 && ostatok <= 4)
+      return krokodola
+    end
 
-		if (ostatok > 4 || ostatok == 0)
-			return krokodilov
-		end
-	end
+    if (ostatok > 4 || ostatok == 0)
+      return krokodilov
+    end
+  end
 end
 
 skolko = ARGV[0].to_i
 
 until (skolko <= 110 && skolko >= 0) do
-	puts "Пожалуйста, введите человеческое число."
-	skolko = STDIN.gets.to_i
+  puts "Пожалуйста, введите человеческое число."
+  skolko = STDIN.gets.to_i
 end
 
 puts "#{skolko} " + 
-	sklonenie(skolko, "негритёнок", "негритёнка", "негритят") + 
-	" " + 
-	sklonenie(skolko, "пошёл", "пошли", "пошли") + 
-	" купаться в море!"
+  sklonenie(skolko, "негритёнок", "негритёнка", "негритят") + 
+  " " + 
+  sklonenie(skolko, "пошёл", "пошли", "пошли") + 
+  " купаться в море!"
 
 #based on the book of Agatha Christie
