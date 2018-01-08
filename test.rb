fa713538dfc2685c932d645b15c59a19520097ca
apperen <a.p4nova@yandex.ru>
2018-01-09 00:00:53 +0300

    test

--- test.rb
+++ test.rb
@@ -1,19 +1,24 @@
 #test
 
-a = "ABC"
-b = "D"
+# a = "ABC"
+# b = "D"
 
-c = a + b
+# c = a + b
 
-puts c
+# puts c
 
 
-array = [1, 2, 3, 4, 5]
-puts array.size
+# array = [1, 2, 3, 4, 5]
+# puts array.size
 
-puts
-puts
+# puts
+# puts
 
-number = gets.chomp
-number.split("-")
-puts.number
+# number = gets.chomp
+# number.split("-")
+# puts.number
+
+
+aa = 50
+bb = aa + (aa/2)
+puts bb
