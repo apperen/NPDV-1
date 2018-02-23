1d62be1579dabfd7f68ee6d3eb52cbd5ac2287e5
apperen <a.p4nova@yandex.ru>
2018-02-23 16:51:54 +0300

    test

--- test.rb
+++ test.rb
@@ -1,32 +1,16 @@
-#test
-array = [8371, 5980, 3825, 14502, 21385, 92730]
+# #test
+# array = [8371, 5980, 3825, 14502, 21385, 92730]
 
-for item in array do
-	if ((item % 15) == 0)
-		puts "Число #{item} делится на 15."
-	end	
-end
-
-# variable = [
-# "w",
-# "h",
-# "a",
-# "t",
-# "'",
-# "s",
-# " ",
-# "g",
-# "o",
-# "i",
-# "n",
-# "g",
-# " ",
-# "o",
-# "n",
-# "?"
-# ]
+# for item in array do
+# 	if ((item % 15) == 0)
+# 		puts "Число #{item} делится на 15."
+# 	end	
+# end
+# # variable = [# "w",# "h",# "a",# "t",# "'",# "s",# " ",# "g",# "o",# "i",# "n",# "g",# " ",# "o",# "n",# "?"# ]
    
- variable = "{what's going on?:;##754658$}"
- var = variable.gsub(/[^\d]/, '').to_i
- puts var
-# puts perem.join(",")
+#  variable = "{what's going on?:#754658$}"
+#  var = variable.gsub(/[^\d]/, '').to_i
+#  puts var # puts perem.join(",")
+puts "enter word"
+number = gets.to_s
+puts number.downcase
