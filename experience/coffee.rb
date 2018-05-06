1a8a14a39d04468c94590607d888627e258d5969
apperen <a.p4nova@ya.ru>
2018-05-06 13:46:20 +0300

    Характеристики кофе

---
+++ experience/coffee.rb
@@ -0,0 +1,42 @@
+puts "Рады приветствовать Вас в нашем кафе!"
+puts "Пожалуйста, подождите минуту, мы готовим кофе."
+
+
+variety = nil #вид черный или разбавленный
+temperature = nil #горячий или холодный
+taste = nil #вкусныйили невкусный
+packaging = nil #c cобой или без упаковки
+
+choice = rand(10)
+if (choice >= 0 && choice <= 3) 
+	variety = "черный"
+else
+	variety = "разбавленный"
+end
+
+choice = rand(10)
+if (choice >= 0 && choice <= 3) 
+	temperature = "горячий"
+else
+	temperature = "холодный"
+end
+
+choice = rand(10)
+if (choice >= 0 && choice <= 3) 
+	taste = "вкусный"
+else
+	taste = "невкусный"
+end
+
+choice = rand(10)
+if (choice >= 0 && choice <= 3) 
+	packaging = "с собой"
+else
+	packaging = "без упаковки"
+end
+
+puts "Итог. Кофе получился:"
+puts "1)" + variety
+puts "2)" + temperature
+puts "3)" + taste
+puts "4)" + packaging
