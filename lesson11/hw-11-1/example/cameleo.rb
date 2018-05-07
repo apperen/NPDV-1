e3290c914b7e743b801de9638117c0e847ae374a
apperen <a.p4nova@ya.ru>
2018-05-08 00:03:53 +0300

    homework 11-1 Хамелеон

--- lesson11/hw-11-1/example/cameleo.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-class Cameleo
-  
-  def change_color(color_name)
-    puts "Теперь я #{color_name}"
-  end
-
-end
--- lesson11/hw-11-1/example/main.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-require './cameleo.rb'
-
-cameleo = Cameleo.new
-
-cameleo.change_color 'красный'
-cameleo.change_color 'синий'
-cameleo.change_color 'фиолетовый в крапинку'
