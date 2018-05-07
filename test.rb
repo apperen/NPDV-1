94133fde3eb84293686d3f783c4776776f64db1d
apperen <a.p4nova@ya.ru>
2018-05-08 00:29:28 +0300

    test

--- test.rb
+++ test.rb
@@ -1,13 +1,6 @@
-# #test
-require './lesson11/lesson/bridge.rb' #работает как Include в PHP
-# array = [8371, 5980, 3825, 14502, 21385, 92730]
+# # #test
+# require './lesson11/lesson/bridge.rb' #работает как Include в PHP
 
-# day_week = hui.wday
-	
-# if (day_week == 6 || day_week == 7)
-# 	puts "+"
-# else
-# 	puts "-"
-# end
-bridge = Bridge.new 
-bridge.open
+arr = ["Крутой Уокер"]
+arr.split(" ")
+puts arr.to_s
