3551778b1c5f7baf02bc2e6d12b90391b96af961
apperen <a.p4nova@ya.ru>
2018-05-08 00:15:31 +0300

    test

--- test.rb
+++ test.rb
@@ -2,21 +2,6 @@
 require './lesson11/lesson/bridge.rb' #работает как Include в PHP
 # array = [8371, 5980, 3825, 14502, 21385, 92730]
 
-# for item in array do
-# 	if ((item % 15) == 0)
-# 		puts "Число #{item} делится на 15."
-# 	end	
-# end
-# # variable = [# "w",# "h",# "a",# "t",# "'",# "s",# " ",# "g",# "o",# "i",# "n",# "g",# " ",# "o",# "n",# "?"# ]
-   
-#  variable = "{what's going on?:#754658$}"
-#  var = variable.gsub(/[^\d]/, '').to_i
-#  puts var # puts perem.join(",")
-# puts "enter word"
-# number = gets.to_s
-# number.downcase!
-# puts number
-
 # day_week = hui.wday
 	
 # if (day_week == 6 || day_week == 7)
