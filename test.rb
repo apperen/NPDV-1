e5b291908afc64c91ec42a630bc2f0638bdef1c5
apperen <a.p4nova@ya.ru>
2018-05-09 17:40:49 +0300

    test

--- test.rb
+++ test.rb
@@ -1,6 +1,11 @@
 # # #test
 # require './lesson11/lesson/bridge.rb' #работает как Include в PHP
 
-arr = ["Крутой Уокер"]
-arr.split(" ")
+arr = ["A", "F", "V", "T", "F", "F"]
+
+puts "Большой массив"
 puts arr.to_s
+puts
+
+arr.downcase
+
