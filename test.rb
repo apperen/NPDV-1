a50b26cafff49719dcc010fe48826e5962b5ccc0
apperen <a.p4nova@ya.ru>
2018-05-15 21:14:19 +0300

    test

--- test.rb
+++ test.rb
@@ -1,11 +1,5 @@
 # # #test
 # require './lesson11/lesson/bridge.rb' #работает как Include в PHP
-
-arr = ["A", "F", "V", "T", "F", "F"]
-
-puts "Большой массив"
-puts arr.to_s
-puts
-
-arr.downcase
-
+start = "  "
+puts start.strip.length
+puts start.length
